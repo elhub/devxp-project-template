0c7c28db7f1cc8c56174f6e22b530e6f3f7e4e48
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-14 14:27:10 +0100

    Add gradle file templates

---
+++ resources/gradleFiles/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("no.elhub.devxp.kotlin-library") version "0.1.6"
+}
+
+group = "no.elhub.yourgroup"
+description = "Description of your project"
+
+repositories {
+    maven("https://jfrog.elhub.cloud/artifactory/elhub-mvn")
+}
+
+dependencies {
+}
